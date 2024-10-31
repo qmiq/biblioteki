80825ee928ede04b05115980ce0dcd4fe5626e7a
Kulemin Artem <moscowtema5@gmail.com>
2024-10-31 18:17:36 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication10/ConsoleApplication10.cpp
@@ -0,0 +1,16 @@
+﻿﻿#include <iostream>
+#include "Kulemin.h"
+#include <windows.h>
+#include <string>
+#include "biblioteki.h"
+using namespace std;
+int main()
+{
+	int arr[6] = { 9, 32, 3, 43, 5, 1 };
+	int r = size(arr);
+	sort_arr(arr[r], int n);
+	int org[5] = { 1, 2, 3, 4, 5 };
+	const int size = sizeof(org) / sizeof(org[0]);
+	int dupli[size];
+	dupl_arr(org[5], dupli[])
+}
